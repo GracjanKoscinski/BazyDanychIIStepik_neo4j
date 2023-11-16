e777b64ed8f502c97551e5798f9885fb2435dfda
Gracjan Koscinski <gracek247927@gmail.com>
2023-11-16 16:22:51 +0100

    added new .gitignore rules

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
+uri = os.getenv('URI')
+user = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+driver = GraphDatabase.driver(uri, auth=(user, password),database="neo4j")
+
 @app.route('/')
 def hello_world():
     return "Hello World"
